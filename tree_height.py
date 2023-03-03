f98d2dc4f135a5591da1489ea3578940515dfda3
Ričards <Ricards.helmanis@edu.rtu.lv>
2023-03-03 16:45:59 +0200

    Prints the function, should catch all exceptions

--- tree_height.py
+++ tree_height.py
@@ -17,30 +17,25 @@ def main():
 
     if input_method == "I":
         n = int(input())
-        parents = input()
-        parents = parents.split()
-        print(n, parents)
+        parents = numpy.fromstring(input(), dtype = int, sep=" ")
     
     elif input_method == "F":
         file_name = input("File name: ")
         if file_name.__contains__("a"):
             print("Input error")
             return
-        with open("test/"+file_name, "r") as f:
-            n = int(f.readline())
-            parents = numpy.fromstring(f.readline(), dtype = int, sep = " ")
-        print(n, parents)
+        try:
+            with open("test/" + file_name, "r") as f:
+                n = int(f.readline())
+                parents = numpy.fromstring(f.readline(), dtype = int, sep = " ")
+        except FileNotFoundError:
+            print("File not found")
 
     else:
         print("Input error")
         return
-
-    # implement input form keyboard and from files
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    
+    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
