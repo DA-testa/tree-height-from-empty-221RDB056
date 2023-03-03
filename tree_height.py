e4681fc46d2792c9feacde8bfd87e07e8a79f4e0
Ričards <Ricards.helmanis@edu.rtu.lv>
2023-03-03 15:11:15 +0200

    Implemented input methods in main

--- tree_height.py
+++ tree_height.py
@@ -14,14 +14,25 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
-    
+    input = input("From keyboard (I) or from files (F): ")
+    if input == "I":
+        nodes = input()
     # let user input file name to use, don't allow file names with letter a
+    elif input == "'F":
+        file_name = input("File name: ")
+        file = open(file_name, "r")
+        if file_name.__contains__("a"):
+            return
+        nodes = file.read()
+        print(nodes)
+
+
+
     # account for github input inprecision
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
